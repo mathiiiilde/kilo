33892ebf7de683dbb63709e23f41fb6f59ee96af
Mathilde Bargoin <mathilde.bargoin@gmail.com>
2017-04-29 18:34:33 +0200

    disable ctrl+c/z/s/q/v/m

--- kilo.c
+++ kilo.c
@@ -15,8 +15,11 @@ void disableRawMode() {
 void enableRawMode() {
 	tcgetattr(STDIN_FILENO, &orig_termios);
 	atexit(disableRawMode);
+
 	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO | ICANON);
+	raw.c_iflag &= ~(ICRNL | IXON);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
